6b3605e39034e3e0d603ae613451c688eccd4a18
Caleb Turner <turner632@morrisville.edu>
2022-02-08 14:53:03 -0500

    Introducing Variables

---
+++ Assets/Scripts/Driver.cs
@@ -0,0 +1,21 @@
+using System.Transactions;
+//using System.Threading.Tasks.Dataflow;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, 0.1f);
+        transform.Translate(0, 0.01f, 0);
+    }
+}
